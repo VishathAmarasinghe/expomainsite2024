e15933da34cc0797a5e31c47b8830fbc3ccbdc38
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2024-02-13 14:22:28 +0530

    Added paddings

--- src/OrganizingCommitee/OC.jsx
+++ src/OrganizingCommitee/OC.jsx
@@ -3,7 +3,7 @@ import Slidertwo from '../Components/ImageSlider/Slidertwo'
 
 const OC = () => {
   return (
-    <div id='ourteam' className="w-full h-2/3 flex flex-col items-center justify-center bg-cover bg-center bg-[url('./assets/whitebg.png')] ">
+    <div id='ourteam' className="w-full pt-12 pb-8 h-2/3 flex flex-col items-center justify-center bg-cover bg-center bg-[url('./assets/whitebg.png')] ">
         <h1 className='text-[#B8915A] font-poppins mt-10 mb-9 text-4xl font-bold'>Our Team</h1>
         <div data-aos="fade-up" className='w-full  h-2/3 mb-8 flex flex-col items-center justify-center'>
             <Slidertwo/>
